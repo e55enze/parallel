48e83bc5aab886fd1bbe4550e6cc8d0a48217e1b
e55enze <un7e3l@gmail.com>
2024-05-27 02:36:20 +0500

    C

---
+++ lab1/loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void loop(float a[], float b[], float c[]){
+    for (int i = 0; i < 4; i++){
+        c[i] = a[i] * b[i];
+    }
+}
+
+int main(int argc, char** argv){
+    int iteratons_num = atoi(argv[1]);
+    float a[4] = {300.0, 4.0, 4.0, 12.0};
+    float b[4] = {1.5, 2.5, 3.5, 4.5};
+    float c[4] = {0.0, 0.0, 0.0, 0.0};
+
+    for (int i = 0; i < iteratons_num; i++){
+        loop(a,b,c);
+    }
+    return 0;
+}
---
+++ lab3/openmp.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+int counter = 0;
+
+void *heavy_task() {
+  int limit = 1e8;
+
+  for(int i=0; i < limit; i++){
+    // result += i;
+    sqrt(i);
+  }
+}
+
+
+void openmp(int thread_num){
+#pragma omp parallel for num_threads(thread_num)
+  for (int i = 0; i < thread_num; i++) {
+    heavy_task();
+  }
+}
+
+
+int main(int argc, char** argv){
+    int counter = 0;
+    int threads_num = atoi(argv[1]);
+
+    openmp(threads_num);
+
+    printf("Counter: %d \n",counter);
+
+}
